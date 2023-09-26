5962b47fbd9e2246c02d3f0bd892d4d9d53140e8
naisongwen <2129939+naisongwen@users.noreply.github.com>
2023-09-26 17:51:07 +0800

    Update cors.py

--- backend/middlewares/cors.py
+++ backend/middlewares/cors.py
@@ -5,10 +5,10 @@ origins = [
     "http://localhost:3000",
     "https://quivr.app",
     "https://zhishi.zeabur.app",
+    "https://quivr.zeabur.app",
     "https://www.quivr.app",
     "http://quivr.app",
-    "http://www.quivr.app",
-    "*",
+    "http://www.quivr.app"
 ]
 
 
